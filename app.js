e2b0d02cafcd7e39fb6922eeaf73199be1ebfdb3
Navid Matin <seyedmatin.1@osu.edu>
2016-01-31 17:34:35 -0500

    Quick fixes, now the templating works

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.set('view', '/src/views');
+app.set('views', 'src/views');
 
-app.set('view engine', '.ejs');
-
-app.use(express.static('src/views'));
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
     res.render('index', {title: 'Hello from render', list:['a','b']});
